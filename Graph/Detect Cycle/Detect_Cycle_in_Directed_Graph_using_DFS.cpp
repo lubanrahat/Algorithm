22d97fd9064475c83df8a339dcafb6e6f953f7fc
Luban Rahat <lubanrahat@Lubans-Mac-mini.local>
2024-09-12 03:19:19 +0600

    Detect_Cycle_in_Directed_Graph_using_DFS

---
+++ Graph/Detect Cycle/Detect_Cycle_in_Directed_Graph_using_DFS.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long i64;
+typedef unsigned long long u64;
+typedef unsigned u32;
+#define PI 3.1415926535897932384626
+#define rep(i,n) for(int i = 0; i < n; i++)
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+
+vector<int> adj[100005];
+vector<int> visited(100005,false);
+vector<int> prentArray(100005,-1);
+vector<bool> pathVisited(100005,true);
+bool flag = false;
+
+void DFS(int prent) {
+    visited[prent] = true;
+    pathVisited[prent] = true;
+    for(auto child: adj[prent]) {
+        if(prentArray[child]) {
+            flag = true;
+        }
+        if(!visited[child]) {
+            DFS(child);
+            prentArray[child] = prent;
+        }
+    }
+    pathVisited[prent] = false;
+}
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int vertex,edge;
+    cin>>vertex>>edge;
+
+    rep(i,edge) {
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    int source;
+    cin>>source;
+
+    DFS(source);
+
+    if(flag) {
+        cout<<"Cycle Detect!"<<endl;
+    } else {
+        cout<<"Cycle not Detect!"<<endl;
+    }
+
+    return 0;
+}
